59f93d90ad4cc9b829c4a53eb507662c9f6f96f6
Brittany Walker <70984129+Britt22walk@users.noreply.github.com>
2020-11-09 12:30:35 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 $(function shoppingList() { 
-    $('#js-shopping-list-form').submit((event) => {
-            event.preventDefault();
-            const newItem = $('.js-shopping-list-entry').val();
-            $('.shopping-list').append(`<li>
-                <span class="shopping-item">${newItem}</span>
-                <div class="shopping-item-controls">
-                <button class="shopping-item-toggle">
-              <span class="button-label">check</span>
-                </button>
-             <button class="shopping-item-delete">
-              <span class="button-label">delete</span>
-             </button>
-              </div>
-             </li>`);
-                $('js-shopping-list-entry').val("");
-            
-        });
-  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
-  });
-  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-    $(this).closest('li').remove();
-  });
-  });
-   
-  
-  
-  
-  $(shoppingList);
-  
+  $('#js-shopping-list-form').submit((event) => {
+          event.preventDefault();
+          const newItem = $('#shopping-list-entry').val();
+          $('.shopping-list').append(`<li>
+              <span class="shopping-item">${newItem}</span>
+              <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+              </button>
+           <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+           </button>
+            </div>
+           </li>`);
+              $('js-shopping-list-entry').val("");
+          
+      });
+$('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+  $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+});
+$('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+  $(this).closest('li').remove();
+});
+});
+ 
+
+
+
+$(shoppingList);
